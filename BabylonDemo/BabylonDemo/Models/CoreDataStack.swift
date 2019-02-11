e2a310c03527aadcb4b097e13b956b794a34e5ad
stdemicheli <stdemicheli@gmail.com>
2019-02-11 13:42:51 +0100

    Adds Core Data entities.

---
+++ BabylonDemo/BabylonDemo/Models/CoreDataStack.swift
@@ -0,0 +1,53 @@
+//
+//  CoreDataStack.swift
+//  BabylonDemo
+//
+//  Created by De MicheliStefano on 11.02.19.
+//  Copyright © 2019 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataEntities: String {
+    case Vehicle, Booking
+}
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    func save(context: NSManagedObjectContext) throws {
+        var error: Error?
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                NSLog("Error while saving to persistent store: \(saveError)")
+                error = saveError
+            }
+        }
+        
+        if let error = error { throw error }
+    }
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Feed")
+        container.loadPersistentStores  { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent store: \(error)")
+            }
+        }
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+}
+
+
