512afd49d2a62ad2935270856f68c8ed269aaa12
stdemicheli <stdemicheli@gmail.com>
2019-02-12 18:38:44 +0100

    Adds a simple version of a Feed table view controller that is bound to the view model.

--- BabylonDemo/BabylonDemo/ViewControllers/PostFeed/FeedViewController.swift
+++ BabylonDemo/BabylonDemo/ViewControllers/PostFeed/FeedViewController.swift
@@ -7,24 +7,51 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class FeedViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var barButton: UIBarButtonItem!
+    @IBOutlet weak var tableView: UITableView!
+    private let feedViewModel: FeedViewModel
+    private let disposeBag = DisposeBag()
+    
+    required init?(coder aDecoder: NSCoder) {
+        // TODO: Move to coordinator
+        let loader = FeedLoader(api: FeedAPI(), store: FeedStore())
+        self.feedViewModel = FeedViewModel(loader: loader)
+        super.init(coder: aDecoder)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindToViewModel()
     }
-    */
+    
+    private func bindToViewModel() {
+        // Combine observables for initial fetch and refresh.
+        let fetchPosts = PublishSubject<Void>()
+        // TODO: Change to refresh control.
+        let refreshPosts = barButton.rx.tap
+        let fetchObservable = Observable.of(fetchPosts.asObservable(), refreshPosts.asObservable()).merge()
+        
+        // Setup view model input and generate output.
+        let input = FeedViewModel.Input(fetch: fetchObservable)
+        let output = feedViewModel.transform(input: input)
+        
+        // Bind tableview to the view model's output.
+        output.posts
+            .drive(tableView.rx.items) {
+                (tableView: UITableView, index: Int, element: String) in
+                let cell = UITableViewCell(style: .default, reuseIdentifier: "PostCell")
+                cell.textLabel?.text = element
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        // Trigger initial fetch.
+        fetchPosts.onNext(())
+    }
 
 }
