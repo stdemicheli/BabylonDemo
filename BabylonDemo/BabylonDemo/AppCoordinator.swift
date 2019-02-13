c139d8cad6d19262e0e8ac9cff791b4a767a0041
stdemicheli <stdemicheli@gmail.com>
2019-02-13 19:10:17 +0100

    Adds an AppCoordinator that manages navigation.

---
+++ BabylonDemo/BabylonDemo/AppCoordinator.swift
@@ -0,0 +1,56 @@
+//
+//  AppCoordinator.swift
+//  BabylonDemo
+//
+//  Created by De MicheliStefano on 13.02.19.
+//  Copyright © 2019 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+
+protocol AppCoordinatorDelegate: class {
+    func didTapOnPost(with postId: Int, userId: Int)
+}
+
+class AppCoordinator: AppCoordinatorDelegate {
+    
+    weak var delegate: AppCoordinatorDelegate?
+    let navigationController: UINavigationController
+    let feedLoader: FeedLoader
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        
+        let feedAPI = FeedAPI()
+        let feedStore = FeedStore()
+        feedLoader = FeedLoader(api: feedAPI, store: feedStore)
+    }
+    
+    // MARK: - Navigation
+    
+    func start() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let feedViewModel = FeedViewModel(loader: feedLoader)
+        let feedViewController = storyboard.instantiateViewController(withIdentifier: "Feed") as! FeedViewController
+        feedViewController.feedViewModel = feedViewModel
+        feedViewController.appCoordinatorDelegate = self
+        
+        navigationController.pushViewController(feedViewController, animated: true)
+    }
+    
+    func showPostDetails(for postId: Int, userId: Int) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let postDetailViewModel = PostDetailViewModel(loader: feedLoader, postId: postId, userId: userId)
+        let postDetailViewController = storyboard.instantiateViewController(withIdentifier: "PostDetail") as! PostDetailViewController
+        postDetailViewController.postDetailViewModel = postDetailViewModel
+        
+        navigationController.pushViewController(postDetailViewController, animated: true)
+    }
+    
+    // MARK: - AppCoordinatorDelegate
+    
+    func didTapOnPost(with postId: Int, userId: Int) {
+        showPostDetails(for: postId, userId: userId)
+    }
+    
+}
--- BabylonDemo/BabylonDemo/AppDelegate.swift
+++ BabylonDemo/BabylonDemo/AppDelegate.swift
@@ -12,10 +12,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var appCoordinator: AppCoordinator!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
         
+        let navigationController = UINavigationController()
+        window?.rootViewController = navigationController
+        
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator.start()
+        
+        window?.makeKeyAndVisible()
         
         return true
     }
